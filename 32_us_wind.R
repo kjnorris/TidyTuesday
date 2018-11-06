a2df334c116ab4ac48adb04c2391f9f520ea8128
Kenneth Norris <kjnorris@gmail.com>
2018-11-06 10:31:10 -0500

    Clean column names
    Changed to descriptive column names in data extract

--- 32_us_wind.R
+++ 32_us_wind.R
@@ -4,5 +4,7 @@ us_wind <- read_csv('./Data/us_wind.csv')
 
 us_wind %>%
   distinct(t_fips, .keep_all = TRUE) %>%
-  select(t_state, t_county, p_year, p_tnum, p_cap, xlong, ylat)
+  select(state = t_state, county = t_county,
+         year = p_year, turbines = p_tnum,
+         capacity = p_cap, lng = xlong, lat = ylat)
 
