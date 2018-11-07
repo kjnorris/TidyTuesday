54587b1b565bdbe9821e9e9d41c682d27590cd48
Ken Norris <kjnorris@gmail.com>
2018-11-07 15:05:23 -0500

    Added two axis graph showing change in total output and turbine efficiency since 2000

--- 32_us_wind.R
+++ 32_us_wind.R
@@ -8,14 +8,16 @@ wind_cap <- us_wind %>%
   distinct(t_fips, .keep_all = TRUE) %>%
   select(state = t_state, county = t_county,
          year = p_year, turbines = p_tnum,
-         capacity = p_cap, lng = xlong, lat = ylat) %>%
+         capacity = p_cap, turbine = t_cap,
+         lng = xlong, lat = ylat) %>%
   filter(state != "AK" & state != "HI" & state != "PR" & state != "GU") %>%
-  filter(capacity > 0) %>%
+  filter(capacity > 0) %>% filter(turbine > 0) %>%
   mutate(decade = case_when(year >= 1980 & year < 1990 ~ "1980s",
                             year >= 1990 & year < 2000 ~ "1990s",
                             year >= 2000 & year < 2010 ~ "2000s",
                             year >= 2010               ~ "2010s",
-                            TRUE                       ~ "Unknown"))
+                            TRUE                       ~ "Unknown")) %>%
+  mutate(turbine = turbine / 10)
 
 wind_unknown <- us_wind %>%
   distinct(t_fips, .keep_all = TRUE) %>%
@@ -51,4 +53,26 @@ ggmap(map) +
        subtitle = "Installed growth by decade and capacity",
        y="",
        x="",
+       caption = "Source: usgs.gov")
+
+wind_cap %>%
+  group_by(year) %>%
+  filter(year <= 2017 & year >= 2000) %>%
+  summarise(annual_capacity = sum(capacity),
+            average_turbine = mean(turbine)) %>%
+  ggplot(aes(x=year)) +
+  geom_bar(aes(y=annual_capacity), stat="identity", fill="#3333FF") +
+  geom_line(aes(y=average_turbine), color="#990000") +
+  expand_limits(y = c(0, 7000), x = c(2000, 2017)) +
+  scale_y_continuous(breaks = seq(0, 7000, 1000),
+                     sec.axis = sec_axis(~.,
+                                         name="Average Turbine Capacity (kW)",
+                                         breaks = seq(0, 3000, 1000),
+                                         labels = seq(0, 3000, 1000)),) +
+  scale_x_continuous(breaks=seq(2000, 2017, 3)) +
+  theme_minimal() +
+  theme(legend.position = "top", legend.direction = "horizontal") +
+  labs(title = "Rising Output and Efficiency Since 2000",
+       subtitle = "Total capacity installed and average turbine output per year",
+       y="Total Capacity (MW)", x="",
        caption = "Source: usgs.gov")
